749356e3bfb76c28e0f85f5a3e6a01abffb25f47
Cherifi-Houdaifa <houdaifacherifi618@gmail.com>
2022-12-16 23:36:20 +0100

    Finished the dialog box of sign up in the auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Auth.css";
 
 export default function Auth () {
+	const dialog = useRef();
 	return <main className="auth">
 		<article className="text">
 			<div>
@@ -15,9 +16,20 @@ export default function Auth () {
 				<input type="text" placeholder="Username" autoComplete="off" />
 				<input type="text" placeholder="Password" autoComplete="off" />
 				<input type="button" value="Sign in" />
-				<input type="button" value="Sign up" />
+				<input type="button" value="Sign up" onClick={(e) => {
+					dialog.current.classList.toggle("show-flex");
+				}} />
 				<input type="button" value="Sign in with google" />
 			</div>
+			<dialog ref={dialog}>
+				<div>
+					<h2>Sign up</h2>
+					<input type="text" placeholder="Username" autoComplete="off" />
+					<input type="text" placeholder="Password" autoComplete="off" />
+					<input type="text" placeholder="Confirm Password" autoComplete="off" />
+					<input type="button" value="Sign up" />
+				</div>
+			</dialog>
 		</article>
 	</main>;
 }
